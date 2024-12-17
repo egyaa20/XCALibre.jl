d3f30446e22ceeab5386b6786cc59c75e489c339
Artem <04artemavdeev04@gmail.com>
2024-12-17 23:28:35 +0000

    familiarising myself with source code

---
+++ prototype/dummy_case.jl
@@ -0,0 +1,98 @@
+using XCALibre
+# using CUDA
+
+# backwardFacingStep_2mm, backwardFacingStep_10mm
+grids_dir = pkgdir(XCALibre, "examples/0_GRIDS")
+grid = "quad100.unv"
+mesh_file = joinpath(grids_dir, grid)
+
+mesh = UNV2D_mesh(mesh_file, scale=0.001)
+
+backend = CPU(); activate_multithread(backend)
+mesh_dev = mesh
+# backend = CUDABackend()
+# mesh_dev = adapt(backend, mesh)
+
+velocity = [0.5, 0.0, 0.0]
+nu = 1e-3
+Re = velocity[1]*0.1/nu
+
+model = Physics(
+    time = Steady(),
+    fluid = Fluid{Incompressible}(nu = nu),
+    turbulence = RANS{Laminar}(),
+    energy = Energy{Isothermal}(),
+    domain = mesh_dev
+    )
+
+@assign! model momentum U (
+    Dirichlet(:inlet, velocity),
+    Neumann(:outlet, 0.0),
+    # Dirichlet(:wall, [0.0, 0.0, 0.0]),
+    # Dirichlet(:top, [0.0, 0.0, 0.0]),
+    # Wall(:wall, [0.0, 0.0, 0.0]),
+    # Wall(:top, [0.0, 0.0, 0.0])
+    # Symmetry(:top)
+)
+
+@assign! model momentum p (
+    Neumann(:inlet, 0.0),
+    Dirichlet(:outlet, 0.0),
+    # Neumann(:wall, 0.0),
+    # Neumann(:top, 0.0)
+    # Symmetry(:top, 0.0)
+)
+
+schemes = (
+    U = set_schemes(divergence = Linear),
+    # U = set_schemes(divergence = Upwind),
+    p = set_schemes()
+)
+
+
+solvers = (
+    U = set_solver(
+        model.momentum.U;
+        solver      = BicgstabSolver, # BicgstabSolver, GmresSolver
+        preconditioner = Jacobi(), # ILU0GPU, Jacobi, DILU
+        # smoother=JacobiSmoother(domain=mesh_dev, loops=5, omega=2/3),
+        convergence = 1e-7,
+        relax       = 0.8,
+        rtol = 1e-1
+    ),
+    p = set_solver(
+        model.momentum.p;
+        solver      = CgSolver, # BicgstabSolver, GmresSolver, CgSolver
+        preconditioner = Jacobi(), # IC0GPU, Jacobi, DILU
+        # smoother=JacobiSmoother(domain=mesh_dev, loops=5, omega=2/3),
+        convergence = 1e-7,
+        relax       = 0.2,
+        rtol = 1e-2
+    )
+)
+
+runtime = set_runtime(
+    iterations=1000, time_step=1, write_interval=100)
+    # iterations=1, time_step=1, write_interval=1)
+
+# hardware = set_hardware(backend=CUDABackend(), workgroup=32)
+hardware = set_hardware(backend=backend, workgroup=1024)
+
+config = Configuration(
+    solvers=solvers, schemes=schemes, runtime=runtime, hardware=hardware)
+
+GC.gc()
+
+initialise!(model.momentum.U, velocity)
+initialise!(model.momentum.p, 0.0)
+
+
+@time residuals = run!(model, config)
+
+using Plots
+iterations = runtime.iterations
+plot(yscale=:log10, ylims=(1e-7,1e-1))
+plot!(1:iterations, residuals.Ux, label="Ux")
+plot!(1:iterations, residuals.Uy, label="Uy")
+plot!(1:iterations, residuals.Uz, label="Uz")
+plot!(1:iterations, residuals.p, label="p")
--- src/Discretise/Discretise_1_schemes.jl
+++ src/Discretise/Discretise_1_schemes.jl
@@ -6,9 +6,28 @@ cID - Index of the cell outer loop. Use to index "b"
 cIndex - Index of the cell based on sparse matrix. Use to index "nzval_array"
 =#
 
+
+function calculate_distance(point1::SVector{N, T}, point2::SVector{N, T}, method::String = "euclidean") where {N, T <: Real}
+    if method == "euclidean"
+        return norm(point1 - point2)
+    elseif method == "manhattan"
+        return sum(abs.(point1 - point2)) 
+    elseif method == "chebysev"
+        return maximum(abs.(point1 - point2))
+    elseif method == "euclidean_squared"
+        return dot(point1 - point2, point1 - point2)
+
+        # return sum((point1 - point2).^2)*0.5
+    else
+        error("Unsupported method: $method. Use \"euclidean\" or \"manhattan\".")
+    end
+end
+
+
+
 # TIME 
 
-# SteadyState
+                # SteadyState
 @inline function scheme!(
     term::Operator{F,P,I,Time{SteadyState}}, 
     nzval_array, cell, face,  cellN, ns, cIndex, nIndex, fID, prev, runtime)  where {F,P,I}
@@ -20,7 +39,7 @@ end
     0.0, 0.0
 end
 
-## Euler
+                    ## Euler
 @inline function scheme!(
     term::Operator{F,P,I,Time{Euler}}, 
     nzval_array, cell, face,  cellN, ns, cIndex, nIndex, fID, prev, runtime)  where {F,P,I}
@@ -45,7 +64,7 @@ end
     nzval_array, cell, face,  cellN, ns, cIndex, nIndex, fID, prev, runtime
     )  where {F,P,I}
 
-    (; area, normal, delta, e) = face
+    (; area, normal, delta, e) = face # maybe define delta differently e.g. not perpendicular across the face but somehow else
     Sf = ns*area*normal
     e = ns*e
     Ef = ((Sf⋅Sf)/(Sf⋅e))*e
@@ -53,7 +72,6 @@ end
     ap = term.sign*(term.flux[fID] * Ef_mag)/delta
 
     # ap = term.sign*(term.flux[fID] * area)/delta
-    
     # Increment sparse array
     ac = -ap
     an = ap
@@ -71,14 +89,29 @@ end
     term::Operator{F,P,I,Divergence{Linear}}, 
     nzval_array, cell, face, cellN, ns, cIndex, nIndex, fID, prev, runtime
     )  where {F,P,I}
+
     # Retrieve mesh centre values
     xf = face.centre
     xC = cell.centre
     xN = cellN.centre
-    
+
+    # [weighted scheme]
     # Calculate weights using normal functions
-    weight = norm(xf - xC)/norm(xN - xC)
-    one_minus_weight = one(eltype(weight)) - weight
+    # weight = norm(xf - xC)/norm(xN - xC) 
+    # one_minus_weight = one(eltype(weight)) - weight 
+
+    # # Calculate required increment
+    # ap = term.sign*(term.flux[fID]*ns)
+    # ac = ap*one_minus_weight
+    # an = ap*weight
+    # return ac, an
+
+
+    # Calculate weights using normal functions
+    # weight = calculate_distance(xf, xC, "euclidean")/calculate_distance(xN, xC, "euclidean")
+
+    weight = face.weight
+    one_minus_weight = one(eltype(weight)) - weight 
 
     # Calculate required increment
     ap = term.sign*(term.flux[fID]*ns)
@@ -97,6 +130,7 @@ end
     nzval_array, cell, face, cellN, ns, cIndex, nIndex, fID, prev, runtime
     )  where {F,P,I}
     # Calculate required increment
+    println("[SCHEME NAME] > Upwind")
     ap = term.sign*(term.flux[fID]*ns)
     ac = max(ap, 0.0) 
     an = -max(-ap, 0.0)
@@ -113,6 +147,7 @@ end
     nzval_array, cell, face, cellN, ns, cIndex, nIndex, fID, prev, runtime
     )  where {F,P,I}
     # Retrieve mesh centre values
+    println("[SCHEME NAME] > LUST")
     xf = face.centre
     xC = cell.centre
     xN = cellN.centre
@@ -138,10 +173,13 @@ end
 
 # BoundedUpwind
 @inline function scheme!(
+    
     term::Operator{F,P,I,Divergence{BoundedUpwind}}, 
     nzval_array, cell, face, cellN, ns, cIndex, nIndex, fID, prev, runtime
     )  where {F,P,I}
     # Calculate required increment
+    println("[SCHEME NAME] > BoundedUpwind")
+
     volume = cell.volume
     ap = term.sign*(term.flux[fID]*ns)
     ac = max(ap, 0.0) - term.flux[fID]#*volume 
@@ -163,7 +201,8 @@ end
 end
 @inline scheme_source!(
     term::Operator{F,P,I,Si}, cell, cID, cIndex, prev, runtime)  where {F,P,I} = begin
-    
+    println("[SCHEME NAME] > IMPLICIT SOURCE")
+
     # Retrieve and calculate flux for cell 
     flux = term.sign*term.flux[cID]*cell.volume # indexed with cID
     ac = flux # indexed with cIndex
