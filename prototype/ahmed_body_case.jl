aef23a9dc86ccc23f57a93471565e2a4a16cd943
Artem <egyaa20@nottingham.ac.uk>
2025-04-02 20:12:23 +0100

    first commit in here

---
+++ prototype/ahmed_body_case.jl
@@ -0,0 +1,85 @@
+
+using XCALibre
+using LinearAlgebra
+
+mesh_dev = mesh
+# mesh_dev = adapt(CUDABackend(), mesh) # uncomment to run on GPU
+
+# Inlet conditions
+velocity = [5, 0.0, 0.0]
+noSlip = [0.0, 0.0, 0.0]
+nu = 1e-3
+Re = (0.2*velocity[1])/nu
+display(Re)
+
+model = Physics(
+    time = Steady(),
+    fluid = Fluid{Incompressible}(nu = nu),
+    turbulence = RANS{Laminar}(),
+    energy = Energy{Isothermal}(), 
+    domain = mesh_dev
+    )
+
+@assign! model momentum U ( 
+    Dirichlet(:inlet, velocity),
+    Neumann(:outlet, 0.0),
+    Wall(:cylinder, noSlip),
+    Neumann(:bottom, 0.0),
+    Neumann(:top, 0.0)
+    # Neumann(:front, 0.0),
+    # Neumann(:back, 0.0)
+)
+
+@assign! model momentum p (
+    Neumann(:inlet, 0.0),
+    Dirichlet(:outlet, 0.0),
+    Neumann(:cylinder, 0.0),
+    Neumann(:bottom, 0.0),
+    Neumann(:top, 0.0)
+    # Neumann(:front, 0.0),
+    # Neumann(:back, 0.0)
+    
+)
+
+solvers = (
+    U = set_solver(
+        model.momentum.U;
+        solver      = BicgstabSolver, # BicgstabSolver, GmresSolver
+        preconditioner = Jacobi(),
+        convergence = 1e-7,
+        relax       = 1.0,
+        rtol = 1e-4,
+        atol = 1e-5
+    ),
+    p = set_solver(
+        model.momentum.p;
+        solver      = CgSolver, # BicgstabSolver, GmresSolver
+        preconditioner = Jacobi(), #NormDiagonal(),
+        convergence = 1e-7,
+        relax       = 0.8,
+        rtol = 1e-4,
+        atol = 1e-5
+    )
+)
+
+schemes = (
+    U = set_schemes(divergence=Upwind, gradient=Midpoint),
+    p = set_schemes(gradient=Midpoint)
+    
+)
+
+
+runtime = set_runtime(iterations=500, write_interval=20, time_step=1) 
+
+
+hardware = set_hardware(backend=CPU(), workgroup=1024)
+
+config = Configuration(
+    solvers=solvers, schemes=schemes, runtime=runtime, hardware=hardware)
+
+GC.gc(true)
+
+initialise!(model.momentum.U, velocity)
+initialise!(model.momentum.p, 0.0)
+
+residuals = run!(model, config)
