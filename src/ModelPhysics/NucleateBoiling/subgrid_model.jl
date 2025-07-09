aa9a82257bffddfd7394a7250f5f5a2c63584978
Artem <egyaa20@nottingham.ac.uk>
2025-07-09 20:26:36 +0100

    Updated: subgrid model

--- src/ModelPhysics/NucleateBoiling/subgrid_model.jl
+++ src/ModelPhysics/NucleateBoiling/subgrid_model.jl
@@ -1,4 +1,31 @@
+export boiling_flux
+
+
 g = 9.81
+
+function boiling_flux(T_w::Float64, T_sat::Float64, sigma::Float64, rho_l::Float64, 
+    rho_v::Float64, c_pl::Float64, k_l::Float64, h_lv::Float64, q_con::Float64, alpha_v::Float64)
+    
+    Npp = nucleate_site_density(T_w, T_sat)
+
+    D_dep = bubble_departure_diameter(sigma, rho_l, rho_v)
+
+    f = bubble_departure_frequency(rho_l, rho_v, D_dep)
+
+    t_wait = t_w(f)
+
+    Kq = K_q(2.0, D_dep, Npp) # First arg is F_A = 2
+
+    h_q = h_q(f, t_wait, rho_l, c_pl, k_l, Kq)
+
+    qq = q_q(h_q, T_w, T_sat)
+    qe = q_e(Npp, f, D_dep, rho_v, h_lv)
+
+    q_subgrid = q_sgnb(alpha_v, qq, qe)
+    q_total = q_t(q_con, alpha_v, qq, qe)
+
+    return q_subgrid, q_total
+end
 
 function nucleate_site_density(T_w::Float64, T_sat::Float64)
     m = 185.0
@@ -30,10 +57,10 @@ function q_e(Npp::Float64, f::Float64, D_dep::Float64, rho_v::Float64, h_lv::Flo
 end
 
 function q_q(h_q::Float64, T_w::Float64, T_q::Float64) #qq''
-    return h_q * (T_w - T_q)
+    return h_q * (T_w - T_q) # T_q is usually assumed to equal T_sat
 end
 
-function K_q(F_A::Float64, D_dep::Float64, Npp::Float64)
+function K_q(F_A::Float64, D_dep::Float64, Npp::Float64) #F_A = 2 from article
     return ( F_A * pi * (D_dep^2) * Npp ) / 4
 end
 
@@ -43,12 +70,12 @@ function t_w(f::Float64) #waiting time
 end
 
 
-function total_boiling_flux(q_con::Float64, alpha_v::Float64, q_q::Float64, q_e::Float64)
+function q_t(q_con::Float64, alpha_v::Float64, q_q::Float64, q_e::Float64) # return this I guess??
     return q_con + (1 - alpha_v)*(q_q + q_e) #q_con calculated from CHT
 end
 
 
-function subgrid_boiling_flux(alpha_v::Float64, q_q::Float64, q_e::Float64)
+function q_sgnb(alpha_v::Float64, q_q::Float64, q_e::Float64)
     return (1 - alpha_v)*(q_q + q_e)
 end
 
